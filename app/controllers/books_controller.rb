636d27514cf5bc6551b9566c2f6f4f89c0354122
ChikaLong <longb.0818@gmail.com>
2021-09-20 02:18:45 +0000

    [Add]閲覧数

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,6 +26,7 @@ class BooksController < ApplicationController
     @newbook=Book.new
     @user=@book.user
     @book_comment=BookComment.new
+    impressionist(@book, nil, unique: [:ip_address])
   end
 
   def edit
